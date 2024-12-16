3a17c8340b942a99a4c2f6903b6474ad6786d649
Xiwen Xu <xiwenxu@shein.com>
2024-12-16 20:22:38 +0800

    init

--- sn.js
+++
@@ -1,6 +0,0 @@
-[rewrite_local]
-^https://scm\.sheincorp\.cn/system/login/wechat script-response-body=https://raw.githubusercontent.com/CivelXu/scripts/refs/heads/main/sn_get_token.js
-
-
-[mitm]
-hostname = scm.sheincorp.cn
