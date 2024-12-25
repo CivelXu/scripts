69cba41c5f4ee6a0212f5274256a9d53045f14de
Xiwen Xu <xuxiwen785@163.com>
2024-12-25 20:44:57 +0800

    Delete showstart_app.js

--- showstart_app.js
+++
@@ -1,5 +0,0 @@
-[rewrite_local]
-http-request ^https:\/\/api3\.showstart\.com\/order\/app\/order\/list script-path=https://raw.githubusercontent.com/CivelXu/scripts/refs/heads/main/showstart_app_get_token.js, requires-body=false
-
-[MITM]
-hostname = api3.showstart.com
